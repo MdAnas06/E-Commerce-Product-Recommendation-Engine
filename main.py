e5616bbf42dfb4037a47c15f9b817f7bd256f76e
MdAnas06 <91mdanas@gmail.com>
2026-06-07 17:20:46 +0530

    Step 1: Project setup and structure created

---
+++ main.py
@@ -0,0 +1,19 @@
+# ==============================
+# E-COMMERCE RECOMMENDATION ENGINE
+# Step 1: Project Setup
+# ==============================
+
+def welcome():
+    print("======================================")
+    print("  E-COMMERCE RECOMMENDATION ENGINE")
+    print("  Step 1: Setup Complete")
+    print("======================================\n")
+
+
+def main():
+    welcome()
+    print("Project is running successfully 🚀")
+
+
+if __name__ == "__main__":
+    main()
